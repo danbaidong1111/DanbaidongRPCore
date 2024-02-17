7ddaae6cebcba6ee1546319c8ccd78e296acfd2a
danbaidong <985815270@qq.com>
2024-02-17 22:28:29 +0800

    Add VNDF Importance Sampling

--- ShaderLibrary/ImageBasedLighting.hlsl
+++ ShaderLibrary/ImageBasedLighting.hlsl
@@ -124,6 +124,60 @@ real3 GetSpecularDominantDir(real3 N, real3 R, real perceptualRoughness, real Nd
 // Importance sampling BSDF functions
 // ----------------------------------------------------------------------------
 
+#ifndef GGX_BOUNDED_VNDF_SAMPLING
+// "Bounded VNDF Sampling for Smith–GGX Reflections"
+// Kenta Eto & Yusuke Tokuyoshi - Siggraph Asia 2023
+// This paper further improves the method of Dupuy and Benyoub in the case where the microfacet normal is used for reflection only.
+// It allows many fewer rays to be rejected at high roughness which improves quality. This is particularly noticeable
+// in multi-bounce reflections of rough metals because longer paths can be followed without early termination.
+#define GGX_BOUNDED_VNDF_SAMPLING 0
+#endif
+
+float VisibleGGXPDF(float3 V, float3 H, float a2, bool bLimitVDNFToReflection = true)
+{
+	float NoV = V.z;
+	float NoH = H.z;
+	float VoH = dot(V, H);
+
+	float d = (NoH * a2 - NoH) * NoH + 1;
+	float D = a2 / (PI*d*d);
+	float k = 1.0;
+#if GGX_BOUNDED_VNDF_SAMPLING
+	if (bLimitVDNFToReflection)
+	{
+		float s = 1.0f + length(V.xy);
+		float s2 = s * s;
+		k = (s2 - a2 * s2) / (s2 + a2 * V.z * V.z); // Eq. 5
+	}
+#endif
+	float PDF = 2 * VoH * D / (k * NoV + sqrt(NoV * (NoV - NoV * a2) + a2));
+	return PDF;
+}
+
+float VisibleGGXPDF_aniso(float3 V, float3 H, float2 Alpha, bool bLimitVDNFToReflection = true)
+{
+	float NoV = V.z;
+	float NoH = H.z;
+	float VoH = dot(V, H);
+	float a2 = Alpha.x * Alpha.y;
+	float3 Hs = float3(Alpha.y * H.x, Alpha.x * H.y, a2 * NoH);
+	float S = dot(Hs, Hs);
+	float D = (1.0f / PI) * a2 * Sq(a2 / S);
+	float LenV = length(float3(V.x * Alpha.x, V.y * Alpha.y, NoV));
+	float k = 1.0;
+#if GGX_BOUNDED_VNDF_SAMPLING
+	if (bLimitVDNFToReflection)
+	{
+		float a = saturate(min(Alpha.x, Alpha.y));
+		float s = 1.0f + length(V.xy);
+		float ka2 = a * a, s2 = s * s;
+		k = (s2 - ka2 * s2) / (s2 + ka2 * V.z * V.z); // Eq. 5
+	}
+#endif
+	float Pdf = (2 * D * VoH) / (k * NoV + LenV);
+	return Pdf;
+}
+
 void SampleGGXDir(real2   u,
                   real3   V,
                   real3x3 localToWorld,
@@ -218,6 +272,49 @@ void SampleAnisoGGXVisibleNormal(float2 u,
     VdotH = saturate(dot(localV, localH));
 }
 
+// "Sampling Visible GGX Normals with Spherical Caps"
+// Jonathan Dupuy & Anis Benyoub - High Performance Graphics 2023
+void SampleAnisoGGXVisibleNormalSphericalCaps(float2 u,
+                                 float3 V,
+                                 float3x3 localToWorld,
+                                 float roughnessX,
+                                 float roughnessY,
+                             out float3 localV,
+                             out float3 localH,
+                             out float  VdotH,
+                                 bool bLimitVDNFToReflection)
+{
+    localV = mul(V, transpose(localToWorld));
+
+    // Stretch view direction, Hemisphere Space
+    float3 Vh = normalize(float3(roughnessX * localV.x, roughnessY * localV.y, localV.z));
+    float Phi = (2 * PI) * u.x;
+    float k = 1.0;
+
+#if GGX_BOUNDED_VNDF_SAMPLING
+	if (bLimitVDNFToReflection)
+	{
+		// If we know we will be reflecting the view vector around the sampled micronormal, we can
+		// tweak the range a bit more to eliminate some of the vectors that will point below the horizon
+		float a = saturate(min(roughnessX, roughnessY));
+		float s = 1.0 + length(V.xy);
+		float a2 = a * a, s2 = s * s;
+		k = (s2 - a2 * s2) / (s2 + a2 * V.z * V.z);
+	}
+#endif
+
+    float Z = lerp(1.0, -k * Vh.z, u.y);
+    float SinTheta = sqrt(saturate(1 - Z * Z));
+    float X = SinTheta * cos(Phi);
+    float Y = SinTheta * sin(Phi);
+    float3 H = float3(X, Y, Z) + Vh;
+
+    // Transform the normal back to the Ellipsoid Space
+    localH = normalize(float3(roughnessX * H.x, roughnessY * H.y, max(0.0, H.z)));
+
+    VdotH = saturate(dot(localV, localH));
+}
+
 // GGX vsible normal sampling, isotropic variant
 void SampleGGXVisibleNormal(float2 u,
                             float3 V,
@@ -229,6 +326,20 @@ void SampleGGXVisibleNormal(float2 u,
 {
     SampleAnisoGGXVisibleNormal(u, V, localToWorld, roughness, roughness, localV, localH, VdotH);
 }
+
+// GGX vsible normal Spherical Caps sampling, isotropic variant
+void SampleGGXVisibleNormalSphericalCaps(float2 u,
+                                         float3 V,
+                                         float3x3 localToWorld,
+                                         float roughness,
+                                     out float3 localV,
+                                     out float3 localH,
+                                     out float  VdotH,
+                                         bool bLimitVDNFToReflection = true)
+{
+    SampleAnisoGGXVisibleNormalSphericalCaps(u, V, localToWorld, roughness, roughness, localV, localH, VdotH, bLimitVDNFToReflection);
+}
+
 
 // weightOverPdf return the weight (without the diffuseAlbedo term) over pdf. diffuseAlbedo term must be apply by the caller.
 void ImportanceSampleLambert(real2   u,
@@ -331,6 +442,45 @@ void ImportanceSampleAnisoGGX(real2   u,
     weightOverPdf = 4.0 * Vis * NdotL * VdotH / NdotH;
 }
 
+void ImportanceSampleGGXSphericalCaps(real2   u,
+                              real3   V,
+                              real3x3 localToWorld,
+                              real    roughness,
+                              real    NdotV,
+                          out real3   L,
+                          out real    VdotH,
+                          out real    NdotL,
+                          out real    weightOverPdf)
+{
+    real3 tangentX = localToWorld[0];
+    real3 tangentY = localToWorld[1];
+    real3 N        = localToWorld[2];
+
+    real3 H;
+    real3 localV, localH;
+    SampleGGXVisibleNormalSphericalCaps(u, V, localToWorld, roughness, localV, localH, VdotH);
+    real3 localL = 2.0 * VdotH * localH - localV;
+    L = mul(localL, localToWorld);
+    H = mul(localH, localToWorld);
+
+    real NdotH = saturate(dot(N, H));
+    // Note: since L and V are symmetric around H, LdotH == VdotH
+    VdotH = saturate(dot(V, H));
+    NdotL = saturate(dot(N, L));
+
+    // Importance sampling weight for each sample:
+    // pdf = G1(V) * D(H) / (4 * (N.V));
+    // weight = fr * (N.L) with fr = F(H) * G2(V, L) * D(H) / (4 * (N.L) * (N.V))
+    // weight over pdf is:
+    // weightOverPdf = F(H) * G2(V, L) / G1(V)
+    // F is apply outside the function
+
+    // V = G / (4 * NdotL * NdotV)
+    float G2 = V_SmithJointGGX(NdotL, NdotV, roughness) * 4 * NdotL * NdotV;
+    float G1 = G_MaskingSmithGGX(NdotV, roughness);
+
+    weightOverPdf = G2 / G1;
+}
 // ----------------------------------------------------------------------------
 // Pre-integration
 // ----------------------------------------------------------------------------
